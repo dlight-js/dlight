d92a8a19726bb0121d05c0f2499e287fcbdc45f9
orange04 <512837861@qq.com>
2023-02-02 22:47:44 +0800

    feat: updated

--- apps/tutorial/src/index.ts
+++ apps/tutorial/src/index.ts
@@ -1,4 +1,5 @@
 import {render} from "@dlightjs/dlight";
-import {ToDoApp} from "./ToDoApp/tsd/index.tsd";
+// import {ToDoApp} from "./ToDoApp/tsd/index.tsd";
+import {Test} from "./ToDoApp/jsx/test.jsx"
 
-render("app", ToDoApp)
+render("app", Test)
