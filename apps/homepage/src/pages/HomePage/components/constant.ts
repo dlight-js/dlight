115a1c793bca62875ae8ec14278a8f7ed9489e42
orange04 <512837861@qq.com>
2023-02-07 09:15:08 +0800

    feat: add FeatureBlock

---
+++ apps/homepage/src/pages/HomePage/components/constant.ts
@@ -0,0 +1,26 @@
+export const featureContent = [
+    {
+        icon: '⚡️',
+        name: 'Performant',
+        description: 'Dlight enhances code performance at compile time and directly manipulates the DOM,\ ' +
+            'which makes the execution speed of the code as close to vanilla JavaScript as possible,\ ' +
+            'even with limited optimization knowledge.',
+    },
+    {
+        icon: '🚲',
+        name: 'Simple',
+        description: 'Dlight enhances code performance at compile time and directly manipulates the DOM,\ ' +
+            'which makes the execution speed of the code as close to vanilla JavaScript as possible,\ ' +
+            'even with limited optimization knowledge.',
+    },
+    {
+        icon: '🍼',
+        name: 'Friendly',
+        description: 'Support jsx and jsd(our new domain syntax) to help you write everything in js.',
+    },
+    {
+        icon: '🦋',
+        name: 'Beautiful',
+        description: 'Every line of code in DLight is elegant and beautiful!',
+    },
+]
