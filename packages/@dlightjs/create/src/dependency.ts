e2b1f9e030fa3fd18a6ba42c3448f4a7b7e03bc6
iandxssxx <iandxssxx@gmail.com>
2023-05-26 05:59:09 +0000

    feat: add full create support

---
+++ packages/@dlightjs/create/src/dependency.ts
@@ -0,0 +1,14 @@
+
+export const dependencies = {
+  "@dlightjs/components": "0.4.4",
+  "@dlightjs/decorators": "0.0.11",
+  "@dlightjs/emotion": "0.0.12",
+  "@dlightjs/dlight": "0.4.13",
+  "@dlightjs/types": "0.1.0"
+}
+
+export const devDependencies = {
+  vite: "^4.0.0",
+  "vite-plugin-dlight-transpiler": "0.4.7",
+  typescript: "^4.9.3"
+}
