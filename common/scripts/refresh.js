a2cd1629bbbba252517207b0bdd0c90adbf8bcc8
Duan Yihan <69476139+IanDxSSXX@users.noreply.github.com>
2023-07-11 01:00:38 +0800

    feat: new file structure

--- common/scripts/refresh.js
+++ common/scripts/refresh.js
@@ -3,8 +3,8 @@ const { execSync } = require("child_process")
 if (!process.argv[2]) {
   // dependencies in order
   execSync("pnpm pipeline refresh core", { stdio: "inherit" })
-  execSync("pnpm pipeline refresh plugin", { stdio: "inherit" })
   execSync("pnpm pipeline refresh ecosystem", { stdio: "inherit" })
+  execSync("pnpm pipeline refresh easy-css", { stdio: "inherit" })
 } else {
   execSync(`pnpm --filter ${process.argv[2]} install`, { stdio: "inherit" })
   execSync(`node ./common/scripts/build.js ${process.argv[2]}`, { stdio: "inherit" })
