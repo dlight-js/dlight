8474447bce34a5c615e2a78ec8bb0a8d36351d4d
Duan Yihan <69476139+IanDxSSXX@users.noreply.github.com>
2023-07-11 05:52:14 +0800

    version: 0.9.3

--- apps/example/src/test.view.ts
+++ apps/example/src/test.view.ts
@@ -2,6 +2,7 @@ import { HStack } from "@dlightjs/components"
 import { DLightIcon, DLightIconType, HelpOutlineOutlined } from "@dlightjs/material-icons"
 import { View, $, type CustomNode } from "@dlightjs/dlight"
 import { span, div, css } from "@dlightjs/easy-css"
+import { button, SubView } from "@dlightjs/types"
 
 class TestView extends View {
   onclick() {
@@ -16,6 +17,7 @@ class TestView extends View {
 
   Body() {
     div(this.count)
+      .borderERose100()
     button("+")
       .onclick(this.onclick)
     this.jj()
