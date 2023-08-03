7224d546bb356282db1e14622f9a6397aee19bb0
orange04 <512837861@qq.com>
2023-08-03 13:32:59 +0800

    feat: delete getCata add getAst

--- packages/ecosystem/markit/src/markit.view.ts
+++ packages/ecosystem/markit/src/markit.view.ts
@@ -1,12 +1,12 @@
-import DLight, { type CustomNode, View } from "@dlightjs/dlight"
-import { Prop, required, type Typed, type RequiredProp, div, h1 } from "@dlightjs/types"
+import DLight, { View } from "@dlightjs/dlight"
+import { Prop, required, type Typed, type RequiredProp, div } from "@dlightjs/types"
 import BlockRenderer from "./blockView"
 import { parse } from "@iandx/markit"
 
 class MarkitView extends View {
   /** @prop */
   @Prop _$content: RequiredProp<string> = required
-  @Prop getCatalogue: RequiredProp<any> = required
+  @Prop getAst: RequiredProp<(any) => void> = required
 
   /** @reactive */
   markitAst: any = parse(this._$content)
@@ -16,9 +16,8 @@ class MarkitView extends View {
   /** @function */
 
   /** @lifecycle */
-  didMount(_els: HTMLElement[], _node: CustomNode): void {
-    console.log(this.markitAst)
-    this.getCatalogue(this.markitAst.filter(paragraph => paragraph.type === "Heading"))
+  didMount() {
+    this.getAst(this.markitAst)
   }
 
   /** @view */
