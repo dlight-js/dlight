e73bbdace288ab44086ed8147605b081f6d8b71f
Duan Yihan <69476139+IanDxSSXX@users.noreply.github.com>
2023-10-20 13:13:23 -0400

    feat: add custom devtools

--- packages/core/dlight-dev/src/Nodes/MutableNode/IfNode.ts
+++ packages/core/dlight-dev/src/Nodes/MutableNode/IfNode.ts
@@ -1,5 +1,8 @@
-import DLight from "@dlightjs/dlight"
+import DLight, { type DLNode } from "@dlightjs/dlight"
 
 export class IfNode extends DLight.IfNode {
-
+  _$bindNewNodes(nodes: DLNode[]): void {
+    super._$bindNewNodes(nodes)
+    window.updateNodes?.()
+  }
 }
