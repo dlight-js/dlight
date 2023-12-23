1e829c7d2f593efb15443c0f444b9ab77bd0f79d
Duan Yihan <69476139+IanDxSSXX@users.noreply.github.com>
2023-12-22 23:16:29 -0500

    feat: init example app

---
+++ apps/example/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite"
+import dlight from "../../packages/tools/vite-plugin-dlight/dist"
+
+export default defineConfig({
+  server: {
+    port: 4320
+  },
+  base: "",
+  plugins: [
+    dlight({ files: "**/*.view.ts", enableDevTools: true })
+  ]
+  // don't minify
+  // build: {
+  //   minify: false
+  // }
+})
