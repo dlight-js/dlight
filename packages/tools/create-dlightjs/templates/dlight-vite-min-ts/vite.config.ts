d8133b0158d6b3974841162449dda7ea73e1a863
Duan Yihan <69476139+IanDxSSXX@users.noreply.github.com>
2023-12-24 14:42:38 -0500

    feat: ts min template to v1.0.0

--- packages/tools/create-dlightjs/templates/dlight-vite-min-ts/vite.config.ts
+++ packages/tools/create-dlightjs/templates/dlight-vite-min-ts/vite.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite'
 import dlight from "vite-plugin-dlight"
 
 export default defineConfig({
-    server: {
-        port: 4320
-    },
-    base: '',
     plugins: [
         dlight({ files: "**/*.view.ts" })
     ]
