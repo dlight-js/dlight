3e33b365881ad3753dd1e1b1637b0f6789218106
Duan Yihan <69476139+IanDxSSXX@users.noreply.github.com>
2023-12-23 23:33:24 -0500

    feat: cleanup for 1.0.0-alpha - vite plugin js

--- packages/tools/vite-plugin-dlight-js/src/index.ts
+++ packages/tools/vite-plugin-dlight-js/src/index.ts
@@ -1,6 +1,7 @@
 import { transform } from "@babel/core"
+import { Plugin, TransformResult } from "vite"
 
-export default function() {
+export default function (): Plugin {
   return {
     name: "decorator",
     transform(code: string, id: string) {
@@ -9,11 +10,11 @@ export default function() {
         configFile: false,
         plugins: [
           ["@babel/plugin-proposal-decorators", { version: "legacy" }],
-          ["@babel/plugin-proposal-class-properties", { loose: true }]
+          ["@babel/plugin-proposal-class-properties", { loose: true }],
         ],
         sourceMaps: true,
-        filename: id
-      })
-    }
-  } as any
+        filename: id,
+      }) as TransformResult
+    },
+  }
 }
