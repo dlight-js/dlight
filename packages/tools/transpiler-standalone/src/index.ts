33c2e36ed1bf9494d6e8b45394110329675cac91
Duan Yihan <69476139+IanDxSSXX@users.noreply.github.com>
2024-01-08 16:22:22 -0500

    feat: remove transpiler

--- packages/tools/transpiler-standalone/src/index.ts
+++
@@ -1,8 +0,0 @@
-import * as babel from "@babel/standalone"
-import dlight from "babel-preset-dlight"
-
-export default function transformDLight(code: string) {
-  return babel.transform(code, {
-    presets: [[dlight, { files: "*" }]],
-  }).code
-}
