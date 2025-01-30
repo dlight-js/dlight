8d225d1bad25a50c73e14a09d579430d9b4f8859
Duane Johnson <duane.johnson@gmail.com>
2025-01-29 20:39:47 -0700

    feat: create @dlightjs/bun-plugin for transpiling with Bun.build

---
+++ packages/tools/bun-plugin/CHANGELOG.md
@@ -0,0 +1,1 @@
+# @dlightjs/bun-plugin
---
+++ packages/tools/bun-plugin/src/index.ts
@@ -0,0 +1,35 @@
+import type { BunPlugin } from "bun"
+import { transform } from "@babel/core"
+import dlight, { type DLightOption } from "babel-preset-dlight"
+
+export const dlightPlugin = ({
+  filter = /\.(view|model)\.[tj]s$/,
+  options = {},
+}: {
+  filter: RegExp
+  options: DLightOption
+}): BunPlugin => ({
+  name: "bun-plugin-dlight",
+  setup(build) {
+    build.onLoad({ filter }, async args => {
+      const code = await Bun.file(args.path).text()
+
+      const result = transform(code, {
+        babelrc: false,
+        configFile: false,
+        presets: [[dlight, options]],
+        sourceMaps: true,
+        filename: args.path,
+      })
+
+      if (!result || !result.code) {
+        throw new Error(`bun-plugin-dlight failed to process ${args.path}`)
+      }
+
+      return {
+        contents: result.code,
+        loader: "ts",
+      }
+    })
+  },
+})
