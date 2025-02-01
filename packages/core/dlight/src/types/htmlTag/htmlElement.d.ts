5a85c6a2af860fc32505d76036cc6d6e50ce3143
Duane Johnson <duane@johnsoneight.com>
2025-01-31 19:17:14 -0700

    chore: lint / formatting for htmlElement (#124)

--- packages/core/dlight/src/types/htmlTag/htmlElement.d.ts
+++ packages/core/dlight/src/types/htmlTag/htmlElement.d.ts
@@ -1,11 +1,8 @@
 import { type Properties } from "csstype"
 
 // ---- Used to determine whether X and Y are equal, return A if equal, otherwise B
-type IfEquals<X, Y, A, B> = (<T>() => T extends X ? 1 : 2) extends <
-  T,
->() => T extends Y ? 1 : 2
-  ? A
-  : B
+type IfEquals<X, Y, A, B> =
+  (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2 ? A : B
 
 export type OmitIndexSignature<ObjectType> = {
   // eslint-disable-next-line @typescript-eslint/ban-types
